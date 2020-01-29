5518c3c130e1a6ab72b37e9100555f1a54df3aef
Clark Tozer <clarkwtozer@gmail.com>
2020-01-29 22:16:53 +1100

    begin polling worker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { call, race, take, takeEvery } from "redux-saga/effects";
+import { call, delay, put, race, take, takeEvery } from "redux-saga/effects";
 
 export interface IPollingAction<T, M, E = Error> {
     type: string;
@@ -33,10 +33,57 @@ function* pollingWorker<T, M>(actionSuffix: string, actionStopSuffix: string, ac
 }
 
 function* pollingExecutor<P, M>(
-    { payload: { id, onContinue, request, interval = 10000, onFailure, onFinished } }: IPollingAction<P, M>,
+    { payload: { id, onContinue, request, interval = 10000, onFailure, onFinished }, meta }: IPollingAction<P, M>,
     actionPrefix: string
 ) {
-    // start polling
+    while (true) {
+        try {
+            yield delay(interval);
+            const response = yield call(request);
+            const toContinue: boolean = yield call(onContinue, response);
+
+            if (!toContinue) {
+                if (onFinished) {
+                    yield call(onFinished, response);
+                }
+
+                yield put({
+                    type: `${actionPrefix}_POLL_FINISHED`,
+                    payload: response,
+                    meta: meta || request
+                });
+
+                yield put({
+                    type: `${actionPrefix}_POLL_STOP`,
+                    payload: id,
+                    meta: meta || request
+                });
+            } else {
+                yield put({
+                    type: `${actionPrefix}_POLL_SUCCESS`,
+                    payload: response,
+                    meta: meta || request
+                });
+            }
+        } catch (error) {
+            if (onFailure) {
+                yield call(onFailure, error);
+            }
+
+            yield put({
+                type: `${actionPrefix}_POLL_FAILED`,
+                payload: error,
+                error: true,
+                meta: meta || request
+            });
+
+            yield put({
+                type: `${actionPrefix}_POLL_STOP`,
+                payload: id,
+                meta: meta || request
+            });
+        }
+    }
 }
 
 export function* pollingSaga<T, M>(actionStartSuffix = "_POLL_START", actionStopSuffix = "_POLL_STOP") {
