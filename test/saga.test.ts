453b5306a8f88e23e60b2369723a3fb1f6284cb2
Clark Tozer <clarkwtozer@gmail.com>
2020-02-03 22:09:11 +1100

    test

--- test/saga.test.ts
+++ test/saga.test.ts
@@ -2,6 +2,26 @@ import { expectSaga } from "redux-saga-test-plan";
 import { pollingSaga } from "../src";
 
 const successRequest = () => Promise.resolve([1, 2, 3, 4, 5]);
-const errorRequest = () => Promise.reject("Errored!");
 
-it("API Success Test", () => expectSaga(pollingSaga).run());
+it("API Success Test", () =>
+    expectSaga(pollingSaga)
+        .put({
+            type: "TEST_POLL_FINISHED",
+            payload: [1, 2, 3, 4, 5],
+            meta: successRequest
+        })
+        .put({
+            type: "TEST_POLL_STOP",
+            payload: 1,
+            meta: successRequest
+        })
+        .dispatch({
+            type: "TEST_POLL_START",
+            payload: {
+                id: 1,
+                interval: 100,
+                request: successRequest,
+                onContinue: () => false
+            }
+        })
+        .run());
